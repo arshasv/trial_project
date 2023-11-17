094944798ce223329af23fcd718afa78dc7c25ea
Arsha S <arshas@opentrends.net>
2023-11-17 12:43:22 +0530

    update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask
  
 app = Flask(__name__)
  
-@app.route('/')
+@app.route("/")
 
 def hello_world():
     return 'Hello World'
  
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-
-    app.run()
+    app.run(debug=True,host="0.0.0.0",port=8080)
